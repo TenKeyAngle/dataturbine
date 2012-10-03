96b062240bdb696aab7a7f2866aea3d071707bb5
millermj1@comcast.net <millermj1@comcast.net@794d4657-1347-0410-a318-bdcbb9f8251d>
2012-10-03 13:32:17 +0000

    updating V3.2 release

---
+++ branches/V3.2/Source/Applications/Folder/com/rbnb/rbnbFolder/rbnbFolder.java
@@ -0,0 +1,356 @@
+
+/*
+Copyright 2010 Erigo Technologies LLC
+
+Licensed under the Apache License, Version 2.0 (the "License"); 
+you may not use this file except in compliance with the License. 
+You may obtain a copy of the License at 
+
+http://www.apache.org/licenses/LICENSE-2.0 
+
+Unless required by applicable law or agreed to in writing, software 
+distributed under the License is distributed on an "AS IS" BASIS, 
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+See the License for the specific language governing permissions and 
+limitations under the License.
+*/
+
+package com.rbnb.rbnbFolder;
+
+import java.io.File;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Hashtable;
+
+import com.rbnb.sapi.*;
+import com.rbnb.utility.ArgHandler;
+
+/******************************************************************************
+ * Watch a file folder and if any files in it update, send the new content to
+ * the RBNB server.
+ * <p>
+ *
+ * @author Matthew J. Miller
+ *
+ * @since V3.2
+ * @version 11/02/2010
+ */
+
+/*
+ * Copyright 2010 Erigo Technologies LLC
+ * All Rights Reserved
+ *
+ *   Date      By	Description
+ * MM/DD/YYYY
+ * ----------  --	-----------
+ * 11/02/2010  MJM	Created.
+ *
+ */
+
+public class rbnbFolder extends Thread {
+
+	private static int updateInc = 2;		// update interval (sec)
+	
+	private static String folderName = ".";
+	private static String rbnbServer = "localhost";  // 127.0.0.1 is more DTN than "localhost"?
+	private static String fileFilter="*";
+	private static String sName="Folder";
+	private static String commonName=null;
+	private static boolean retainFile=true;
+	private static boolean sendInitial=false;
+	private static boolean negateFilter=false;
+	private static int Ncache=1, Narchive=10000;
+	private static String Amode="append";
+	
+	private static long iterCount=0;
+	private static Timer myTimer;
+	public static Source source=null;
+	private static long lastMod = 0;
+	private static final String TAG = "rbnbFolder";
+	private static Pattern pattern;
+	private static File folder=null;
+	
+	private static Hashtable fileHash = new Hashtable();
+	private static Hashtable fileHash2 = new Hashtable();
+	final static String FileNames = "_FileNames";
+	final static String CommonName = "commonName";		// hard wire for now
+	
+//---------------------------------------------------------------------------------	
+    public rbnbFolder() {
+    	
+    	myTimer = new Timer();
+    	myTimer.schedule(new TimeTask(), 1000,(int)(updateInc*1000)); 	// initial delay
+    }
+
+//---------------------------------------------------------------------------------	   
+    public static void main (String[] args) {
+
+    	boolean printHelp=false;
+    	
+	    try { 
+		    ArgHandler ah = new ArgHandler(args);
+		    String value;
+		    if ((value = ah.getOption('f')) != null) folderName = value;
+		    if ((value = ah.getOption('a')) != null) rbnbServer = value;
+		    if ((value = ah.getOption('s')) != null) sName = value;
+		    if ((value = ah.getOption('c')) != null) Ncache = Integer.parseInt(value);
+		    if ((value = ah.getOption('n')) != null) Narchive = Integer.parseInt(value);
+		    if ((value = ah.getOption('m')) != null) Amode = value;
+		    if ((value = ah.getOption('F')) != null) fileFilter = value;
+		    if ((value = ah.getOption('C')) != null) commonName=value;
+		    if ((value = ah.getOption('T')) != null) updateInc=Integer.parseInt(value);
+		    if (ah.checkFlag('D')) retainFile=false;
+		    if (ah.checkFlag('I')) sendInitial=true;
+		    if (ah.checkFlag('G')) negateFilter=true;
+		    if (ah.checkFlag('h')) printHelp=true;
+    	} catch (Exception e) {
+    	    System.err.println("Error on argument parsing");
+    	    e.printStackTrace();
+    	    System.exit(-1);
+    	};
+ 
+    	if((args.length == 0) || printHelp) System.out.println(
+	    		"rbnbFolder"+
+	    		"\n -f<folder>      \tdefault="+folderName+ 
+	    		"\n -a<host>        \tdefault="+rbnbServer+
+	    		"\n -s<source>      \tdefault="+sName+
+	    		"\n -c<cache>       \tdefault="+Ncache+
+	    		"\n -n<archive>     \tdefault="+Narchive+
+	    		"\n -m<amode>       \tdefault="+Amode+
+	    		"\n -F<filter>      \tdefault="+fileFilter+
+	    		"\n -C<commonName>  \tdefault="+commonName+
+	    		"\n -T<timeInterval>\tdefault="+updateInc+
+	    		"\n -D<deleteFile>  \tdefault=false"+
+	    		"\n -I<sendInitial> \tdefault=false"+
+	    		"\n -G<negateFilter>\tdefault=false");
+    	
+	    if(printHelp) System.exit(0);
+	    else	System.out.println("Running...");
+	    
+        try {
+        	startSource();			// connect to RBNB server
+        } catch(Exception e) {
+        	e.printStackTrace();
+        	System.exit(0);
+        };
+
+        pattern = Pattern.compile
+        			(replaceWildcards(fileFilter));	// do this once in advance
+    
+    	initializeFolder();			// initialize file hashtable
+        new rbnbFolder();			// start the timer check 
+    }
+    
+//---------------------------------------------------------------------------------	
+// main activity class - check for updates on timer thread
+    
+	  class TimeTask extends TimerTask {
+		    public void run() {
+				FileWatch();
+				iterCount++;
+//				System.err.println("iterCount: "+iterCount);
+		    }
+	  }
+
+//---------------------------------------------------------------------------------	
+	  private static void startSource() {
+	        try {
+	        	if(Amode.equals("create")) {
+	        		try {
+//	        			System.err.println("clearing out old source...");
+	        			source = new Source(Ncache, "append", Narchive);
+	    	        	source.OpenRBNBConnection(rbnbServer,sName);
+	        			Thread.sleep(100);	// let connection close?
+	        			source.CloseRBNBConnection();		// clear up any old source
+	        		} catch (Exception e) {
+//						e.printStackTrace();
+	        		};		// np just continue
+	        	}
+	        	source = new Source(Ncache,Amode,Narchive);	
+	        	source.OpenRBNBConnection(rbnbServer,sName);
+	        	String myName = source.GetClientName();
+	        	if(myName.equals(sName)) 
+	        			System.err.println("Connected: "+myName);
+	        	else	System.err.println("Warning, name mismatch, sName: "+sName+", myName: "+myName);
+	        } catch(Exception e) {
+	        	e.printStackTrace();
+	        	System.exit(0);
+	        };  
+	  }
+	  
+	  private static void stopSource() {
+		    if(source != null) {
+		    	System.err.println("Detaching source");
+		    	source.Detach();
+		    }
+	  }
+
+//---------------------------------------------------------------------------------	
+// initialize folder by building a hash table of file names and modified times
+	  
+	private static void initializeFolder() {
+		int nfound=0;
+		try {
+			// get updated list of files
+	    	folder = new File(folderName);
+			File[] listOfFiles = folder.listFiles();
+			if(listOfFiles == null) return;
+//			System.err.println("folder: "+folderName+", nfile: "+listOfFiles.length);
+			
+			for(int i=0; i<listOfFiles.length; i++) {
+				File file = listOfFiles[i];
+				if(fileMatch(file))	{		// add to initial file hash
+					nfound++;
+					String fileName = new String(file.getName());
+					Long lastMod; 
+					if(sendInitial) lastMod = new Long(0);	// force initial update
+					else			lastMod = new Long(file.lastModified());
+					
+					fileHash.put(fileName, lastMod);
+//					System.err.println("fileHash("+fileName+","+lastMod+")");
+				}
+			}
+		} catch (Exception e) {
+    		System.err.println("Error initializing folder: "+folderName);
+			e.printStackTrace();
+		}
+		
+//		System.err.println("rbnbFolder initialized, matches found: "+nfound);
+	}
+
+//---------------------------------------------------------------------------------	
+// check if a file is legal and passes file-filter
+	
+	private static boolean fileMatch(File file) {
+		if(!file.isFile()) return(false);
+		
+		String fName = file.getName();
+		
+		Matcher matcher = pattern.matcher(fName);
+		if(negateFilter) { if(matcher.matches()) return(false);  }
+		else 			 { if(!matcher.matches()) return(false); }  
+
+		if(file.length() <= 0) return(false);
+
+		return(true);	
+	}
+	
+//---------------------------------------------------------------------------------	
+// do the main file checking and send to RBNB if legal update
+	
+	private synchronized void FileWatch() {
+		try {
+			// get updated list of files
+
+			File[] listOfFiles = folder.listFiles();
+			if(listOfFiles == null) return;
+
+			for(int i=0; i<listOfFiles.length; i++) {
+				File file = listOfFiles[i];
+				String fileName =  file.getName();
+				
+				// fis.methods catch already-open file on Windows, not Unix
+//				if(!canOpen(fileName))continue;		// this method doesn't seem to help on any OS
+				
+				if(fileMatch(file)) {
+//					String fileName =  file.getName();
+					long newMod = file.lastModified();
+					long oldMod = 0;
+					if(fileHash.containsKey(fileName))
+						oldMod = ((Long)fileHash.get(fileName)).longValue();
+					
+//					fileHash.put(fileName, new Long(newMod));	// new/update entry
+					if(newMod > oldMod) {	// it's a go
+//						System.err.println("folder update: "+fileName+", newMod: "+newMod+", oldMod: "+oldMod);
+				
+					// define RBNB channels
+						ChannelMap cMap = new ChannelMap();
+						if(commonName != null) {
+							cMap.Add(commonName);
+							cMap.Add("_FileNames");
+						} else
+							cMap.Add(fileName);
+
+						byte[] data;
+						long fileLength = file.length();
+						data = new byte[(int)(fileLength)];	// read file update
+						java.io.FileInputStream fis = new java.io.FileInputStream(file);
+						int nread = fis.read(data);
+						fis.close();
+						if(nread > 0) {
+							System.err.println("Put file: "+fileName+", size: "+fileLength);
+							cMap.PutDataAsByteArray(0, data);	// always write data to chan0
+							if(commonName != null) {
+								cMap.PutDataAsString(1, fileName+"\n");
+							}
+							source.Flush(cMap);	
+							
+							fileHash.put(fileName, new Long(newMod));	// new/update entry on success
+						}
+	
+						if(!retainFile) {
+							boolean status = file.delete();
+							fileHash.remove(fileName);	// toss known dead files (proactively sweep/clean?)
+							if(status) System.err.println("Deleted source file: "+fileName);
+							else	System.err.println("Failed to delete source file: "+fileName);
+						}
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			if(e instanceof SAPIException) 
+				startSource();		// presume it needs to reconnect?  This can make _1 source...
+		}
+	}
+
+//---------------------------------------------------------------------------------
+	// utility to test if file is available (i.e. not currently being written)
+	// doesnt seem to work bleh
+	
+	private boolean canOpen(String fname) {
+		
+		try {
+			File wfile = new File(fname);
+			java.io.FileOutputStream fos = new java.io.FileOutputStream(wfile);
+			fos.close();
+		}
+		catch(Exception e) {
+			System.err.println("file NG: "+fname);
+			return(false);
+		}
+//		System.err.println("file OK: "+fname);
+		return(true);
+	}
+	
+//---------------------------------------------------------------------------------
+// utility to convert file "glob" to java wildcard logic
+	
+	private static String replaceWildcards(String wild)
+	{
+	    StringBuffer buffer = new StringBuffer();
+	 
+	    char [] chars = wild.toCharArray();
+	 
+	    for (int i = 0; i < chars.length; ++i)
+	    {
+	        if (chars[i] == '*')
+	            buffer.append(".*");
+	        else if (chars[i] == '?')
+	            buffer.append(".");
+	        else if ("+()^$.{}[]|\\".indexOf(chars[i]) != -1)
+	            buffer.append('\\').append(chars[i]);
+	        else
+	            buffer.append(chars[i]);
+	    }
+	 
+	    return buffer.toString();
+	 
+	}// end replaceWildcards method
+
+}
+
+
+
